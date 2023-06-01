864d3979ee0d0925eedd9db953918db82aa73f60
sonvt8 <vuthaison198x@gmail.com>
2023-06-01 11:45:22 +0700

    event bus to query posts

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
+const cors = require('cors');
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const  commentsByPostId = {};
 
@@ -11,16 +14,31 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = crypto.randomBytes(4).toString('hex');
-  const { content } = req.body;
+  const {content} = req.body;
   const comments = commentsByPostId[req.params.id] || [];
 
   comments.push({ id: commentId, content});
 
   commentsByPostId[req.params.id] = comments;
 
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
